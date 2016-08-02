e86b210f9e61b01883a4944694b15d27a95141ae
zhangzequn <776021361@qq.com>
2016-08-02 16:53:36 +0800

    【Change】add some files

---
+++ AugApiSrc/config/mysql-db.js
@@ -0,0 +1,54 @@
+var mysql = require('mysql');
+
+var TEST_DATABASE = 'zzq';
+var TEST_TABLE = 'zzqmain';
+
+//创建连接
+
+module.exports = {
+
+	queryDb: function (tableName) {
+		var client = this.connectDb();
+		client.query('SELECT * FROM ' + tableName,
+			function selectCb(err, results) {
+				if (err) {
+					throw err;
+				}
+				if (results) {
+					for (var i = 0; i < results.length; i++) {
+						console.log("%d\t%s\t%s", results[i].id, results[i].name, results[i].age);
+					}
+				}
+				client.close();
+				client.end();
+			}
+		)
+	},
+	exeDb: function (SQLString,tableName) {
+		if (!tableName) tableName = TEST_TABLE;
+
+		var client = this.connectDb();
+		client.query(SQLString,
+		function selectCb(err, results) {
+			if (err) {
+				throw err;
+			}
+			if (results) {
+
+			}
+			client.close();
+			client.end();
+		})
+	},
+	connectDb: function (tableName) {
+		var client = mysql.createConnection({
+			host: '127.0.0.1',
+			user: 'root',
+			password: '123456',
+			database: 'zzq'
+		})
+		client.connect();
+		return client;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # node_express
-to learn express
+a basic pjt on {nodejs&express&mongodb} writen by a beginner.
+contain some apis for web&phone to use.(under localhost env)

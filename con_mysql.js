8f6c93f718387a03c8c2d015412e0ed2533db582
zhangzequn <776021361@qq.com>
2016-08-15 18:17:38 +0800

    【Change】剔除无用的旧demo代码。

--- con_mysql.js
+++ con_mysql.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var TEST_DATABASE = 'mainlist';
 var TEST_TABLE = 'zzqtable';
 
-//创建连接
+//MYSQL创建连接 实力实例示例!
 var client = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
